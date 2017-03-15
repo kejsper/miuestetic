b67700c1212487299336b47c8873e87c74636e71
kejsper\Programowanie <kejsper@gmail.com>
2017-03-15 16:38:08 +0100

    1. Added scroll top arrow visible/hidden 2. Added slider images adjust their sizes to window size

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 $(document).ready(function () {
-  console.log('dupa');
+  var sliderfit = $('.sliderfit'),
+      scrolltop = $('#scrolltop');
 
+  showScrolltop();
+  adjustSlider();
+
+  $(document).on('scroll', function () {
+    setTimeout(showScrolltop, 300);
+  });
+
+  $(window).on('resize', function () {
+    setTimeout(adjustSlider, 100);
+  });
+
+  function showScrolltop () {
+    var scrollPosition = $(document).scrollTop();
+    if (scrollPosition > 50) {
+      scrolltop.fadeIn(300);
+    }
+    else {
+      scrolltop.fadeOut(300);
+    }
+  }
+
+  function adjustSlider () {
+    var windowWidth = $(window).width() + 17,
+        ratio = 3.047,
+        windowHeight = Math.floor(windowWidth/ratio);
+    sliderfit.height(windowHeight);
+  }
 
 });
