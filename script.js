82335db3610aa83fe876ea434cf1a943dcd51cb8
kejsper\Programowanie <kejsper@gmail.com>
2017-03-15 21:25:19 +0100

    Scroll top arrow working

--- script.js
+++ script.js
@@ -13,6 +13,12 @@ $(document).ready(function () {
     setTimeout(adjustSlider, 100);
   });
 
+  scrolltop.on('click', function (event) {
+    event.preventDefault();
+    $('html, body').animate({scrollTop: 0}, 600);
+  });
+
+  // Shows scroll top arrow
   function showScrolltop () {
     var scrollPosition = $(document).scrollTop();
     if (scrollPosition > 50) {
@@ -22,7 +28,7 @@ $(document).ready(function () {
       scrolltop.fadeOut(300);
     }
   }
-
+  // Adjust height of the images inside slider
   function adjustSlider () {
     var windowWidth = $(window).width() + 17,
         ratio = 3.047,
